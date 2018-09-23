6c2c04c3b7f2ad1ad9d429652baa2742a527edce
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-23 23:17:25 +0200

    Added custom events binding
    Added custom events binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElememtComponent } from './server-elememt/server-elememt.component';
+import { ServerElementComponent } from './server-elememt/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CockpitComponent,
-    ServerElememtComponent
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
