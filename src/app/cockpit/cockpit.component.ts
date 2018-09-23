9d2a0b9f0c2708d44d0a3208a74feba4d23edd0f
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-24 00:55:04 +0200

    Added @ViewChild usage
    Added @ViewChild usage

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -11,7 +11,9 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
 
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
@@ -21,13 +23,13 @@ export class CockpitComponent implements OnInit {
   onAddServer(serverNameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: serverNameInput.value,
-      serverContent: this.newServerContent});
+      serverContent: this.serverContentInput.nativeElement.value});
   }
 
   onAddBlueprint(serverNameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       blueprintName: serverNameInput.value,
-      blueprintContent: this.newServerContent});
+      blueprintContent: this.serverContentInput.nativeElement.value});
   }
 
 }
